3c78e1b86c15916e71e74334d8d04f414a34f207
Jacob Preston <pindrought@gmail.com>
2018-09-07 07:08:50 -0400

    Commit

--- DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/Source.cpp
+++ DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/Source.cpp
@@ -3,10 +3,10 @@
 #pragma comment(lib,"d3d11.lib")
 #pragma comment(lib,"DirectXTK.lib")
 
-int APIENTRY wWinMain(	_In_ HINSTANCE hInstance,
-						_In_opt_ HINSTANCE hPrevInstance,
-						_In_ LPWSTR    lpCmdLine,
-						_In_ int       nCmdShow)
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
 {
 	return 0;
 }
