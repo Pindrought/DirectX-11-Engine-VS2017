a09e09c9d7f8f1c9d2c81e2b22ea5225deac817a
Jacob Preston <pindrought@gmail.com>
2018-09-07 07:07:54 -0400

    Commit

--- DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/Source.cpp
+++ DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/Source.cpp
@@ -1,11 +1,12 @@
+//Tutorial 1 Solution 2018-09-07
 #include <Windows.h>
 #pragma comment(lib,"d3d11.lib")
 #pragma comment(lib,"DirectXTK.lib")
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-	_In_opt_ HINSTANCE hPrevInstance,
-	_In_ LPWSTR    lpCmdLine,
-	_In_ int       nCmdShow)
+int APIENTRY wWinMain(	_In_ HINSTANCE hInstance,
+						_In_opt_ HINSTANCE hPrevInstance,
+						_In_ LPWSTR    lpCmdLine,
+						_In_ int       nCmdShow)
 {
 	return 0;
 }
