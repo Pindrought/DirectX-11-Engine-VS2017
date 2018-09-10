0148def080c9104de721653a3048b403bba8bfda
Jacob Preston <pindrought@gmail.com>
2018-09-10 10:23:06 -0400

    Commit

--- DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/WindowContainer.cpp
+++ DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/WindowContainer.cpp
@@ -2,6 +2,5 @@
 
 LRESULT WindowContainer::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	OutputDebugStringA("WINDOW PROC FROM WINDOW CONTAINER\n");
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
