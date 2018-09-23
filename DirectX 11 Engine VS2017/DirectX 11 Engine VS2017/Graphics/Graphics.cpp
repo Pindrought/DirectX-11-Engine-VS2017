499e0cf8cbf9ddfc3ded25637b4d5ea3fbbd18b1
Jacob Preston <pindrought@gmail.com>
2018-09-23 13:40:19 -0400

    Commit

--- DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/Graphics/Graphics.cpp
+++ DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/Graphics/Graphics.cpp
@@ -111,9 +111,6 @@ bool Graphics::InitializeShaders()
 #endif
 	}
 
-	if (!vertexshader.Initialize(this->device, shaderfolder + L"vertexshader.cso"))
-		return false;
-
 	D3D11_INPUT_ELEMENT_DESC layout[] =
 	{
 		{"POSITION", 0, DXGI_FORMAT::DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA, 0  },
@@ -121,12 +118,11 @@ bool Graphics::InitializeShaders()
 
 	UINT numElements = ARRAYSIZE(layout);
 
-	HRESULT hr = this->device->CreateInputLayout(layout, numElements, this->vertexshader.GetBuffer()->GetBufferPointer(), this->vertexshader.GetBuffer()->GetBufferSize(), this->inputLayout.GetAddressOf());
-	if (FAILED(hr))
-	{
-		ErrorLogger::Log(hr, "Failed to create input layout.");
+
+	if (!vertexshader.Initialize(this->device, shaderfolder + L"vertexshader.cso", layout, numElements))
 		return false;
-	}
+
+	
 
 	return true;
 }
