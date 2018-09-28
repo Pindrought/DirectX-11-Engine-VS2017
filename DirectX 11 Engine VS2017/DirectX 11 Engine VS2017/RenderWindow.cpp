2d06eec405ff250ce4eec9843cba7c3e90fee3df
Jacob Preston <pindrought@gmail.com>
2018-09-28 05:48:04 -0400

    Commit

--- DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/RenderWindow.cpp
+++ DirectX 11 Engine VS2017/DirectX 11 Engine VS2017/RenderWindow.cpp
@@ -12,14 +12,24 @@ bool RenderWindow::Initialize(WindowContainer * pWindowContainer, HINSTANCE hIns
 
 	this->RegisterWindowClass();
 
+	int centerScreenX = GetSystemMetrics(SM_CXSCREEN) / 2 - this->width / 2;
+	int centerScreenY = GetSystemMetrics(SM_CYSCREEN) / 2 - this->height / 2;
+
+	RECT wr; //Widow Rectangle
+	wr.left = centerScreenX;
+	wr.top = centerScreenY;
+	wr.right = wr.left + this->width;
+	wr.bottom = wr.top + this->height;
+	AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE);
+
 	this->handle = CreateWindowEx(0, //Extended Windows style - we are using the default. For other options, see: https://msdn.microsoft.com/en-us/library/windows/desktop/ff700543(v=vs.85).aspx
 		this->window_class_wide.c_str(), //Window class name
 		this->window_title_wide.c_str(), //Window Title
 		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, //Windows style - See: https://msdn.microsoft.com/en-us/library/windows/desktop/ms632600(v=vs.85).aspx
-		0, //Window X Position
-		0, //Window Y Position
-		this->width, //Window Width
-		this->height, //Window Height
+		wr.left, //Window X Position
+		wr.top, //Window Y Position
+		wr.right - wr.left, //Window Width
+		wr.bottom - wr.top, //Window Height
 		NULL, //Handle to parent of this window. Since this is the first window, it has no parent window.
 		NULL, //Handle to menu or child window identifier. Can be set to NULL and use menu in WindowClassEx if a menu is desired to be used.
 		this->hInstance, //Handle to the instance of module to be used with this window
